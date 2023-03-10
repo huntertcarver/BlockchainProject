296bbf46760a2f033fce7805309820c5920f2ea1
unknown <jacobcburns@yahoo.com>
2023-03-09 20:49:27 -0600

    Commented classes and removed P2P network classes

--- src/blockchainprojectpackage/Block.java
+++ src/blockchainprojectpackage/Block.java
@@ -16,11 +16,11 @@ public class Block {
     private int nonce;
   
     public Block(int index, String data, String prevHash) {
-        this.index = index;
-        this.hash = hashFunc();
-        this.time = new Date().getTime();
-        this.data = data;
-        this.prevHash = prevHash;
+        this.index = index; //the Block's current position within the Blockchain
+        this.hash = hashFunc(); //the unique ID assigned to the Block upon generation
+        this.time = new Date().getTime(); //the precise time the Block was created
+        this.data = data; //the actual data stored within the Block
+        this.prevHash = prevHash; //a pointer to the ID of the previous Block on the chain
     }
 
     private String hashFunc() {
@@ -29,10 +29,10 @@ public class Block {
 
         MessageDigest digest;
         try {
-            digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(toBeHashed.getBytes(StandardCharsets.UTF_8));
-            hashedData = Base64.getEncoder().encodeToString(hash);
-        } catch (NoSuchAlgorithmException e) {
+            digest = MessageDigest.getInstance("SHA-256"); //initialize a MessageDigest object which gives access to our one-way hash algorithm, SHA-256
+            byte[] hash = digest.digest(toBeHashed.getBytes(StandardCharsets.UTF_8)); //encodes the string of data into bytes and runs it through SHA
+            hashedData = Base64.getEncoder().encodeToString(hash); //encodes the byte array back into a string
+        } catch (NoSuchAlgorithmException e) { //thrown when SHA is not found in the environment
             e.printStackTrace();
         }
 
@@ -41,13 +41,16 @@ public class Block {
     }
 
     public void mineBlock(int difficulty) {
-        String target = new String(new char[difficulty]).replace('\0', '0');
-        while (!hash.substring(0, difficulty).equals(target)) {
+        String hashPassword = new String(new char[difficulty]).replace('\0', '0'); //creates a password of zeroes based on difficulty (n)
+        while (!hash.substring(0, difficulty).equals(hashPassword)) { //check if the first character to the nth character in the hash is equal to the password of zeroes
             nonce++;
             hash = hashFunc();
+	    //nonce is iterated and the hash function is called again to generate a new hash, which is then checked until the first nth characters equal the password
         }
-        System.out.println("Block Mined : " + hash);
+        System.out.println("Block Mined : " + hash); //succeeds and prints the hash value
     }
+
+    //-------------------Getters and setters for the Block class-------------------
 
     public int getIndex() {
         return index;
@@ -69,12 +72,12 @@ public class Block {
         return prevHash;
     }
 
-    public void setPreviousHash(String prevHash) {
-        this.prevHash = prevHash;
-    }
-
     public String getData() {
         return data;
+    }
+
+    public void setPreviousHash(String prevHash) {
+        this.prevHash = prevHash;
     }
 
     public void setData(String data) {
--- src/blockchainprojectpackage/Blockchain.java
+++ src/blockchainprojectpackage/Blockchain.java
@@ -7,41 +7,45 @@ import java.util.ArrayList;
 import java.util.Base64;
 
 public class Blockchain {
-    private ArrayList<Block> blocks;
-    private int difficulty;
+    private ArrayList<Block> blocks; //Array list to hold blocks, to make it easy to access different transactions.
+    private int difficulty; //number that mining algorithm increases up to (changes how many 0's to check for, the more 0's the more difficult).
 
+    //constructor
     public Blockchain(int difficulty) {
-        this.blocks = new ArrayList<Block>();
-        this.difficulty = difficulty;
+        this.blocks = new ArrayList<Block>(); //dynamically allocate the arraylist class object.
+        this.difficulty = difficulty; //set the difficulty to what was passed when making the object.
         // create genesis block
-        Block genesisBlock = new Block(0, "Genesis Block", "0");
+        Block genesisBlock = new Block(0, "Genesis Block", "0"); //First block in blockchain, does not point to any previous block.
         genesisBlock.mineBlock(difficulty);
         this.blocks.add(genesisBlock);
     }
 
+    //returns the most recent block from the arraylist.
     public Block getLatestBlock() {
         return this.blocks.get(this.blocks.size() - 1);
     }
 
+    //adds a new block to the end of the blockchain.
     public void addBlock(Block newBlock) {
         newBlock.setPreviousHash(this.getLatestBlock().getHash());
         newBlock.mineBlock(difficulty);
         this.blocks.add(newBlock);
     }
 
+    //Makes sure that there are no duplicates hash values, and that all blocks are pointing correctly.
     public boolean isValid() {
         for (int i = 1; i < this.blocks.size(); i++) {
             Block currentBlock = this.blocks.get(i);
             Block previousBlock = this.blocks.get(i - 1);
 
+            //checks if the blocks are in order.
             if (!currentBlock.getPreviousHash().equals(previousBlock.getHash())) {
                 return false;
             }
-            
+            //verifying that the block matches itself
             if (!currentBlock.getHash().equals(hashBlock(currentBlock))) {
                 return false;
             }
-
             if (currentBlock.getIndex() != i) {
                 return false;
             }
