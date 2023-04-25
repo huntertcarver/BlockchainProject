9266c3210d9163c4c10b33f235c281f7e730de65
christian Cormier <43218064+Christian-Cormier@users.noreply.github.com>
2023-04-25 17:42:01 -0500

    Add files via upload
    final test

---
+++ LoginPanel.java
@@ -0,0 +1,80 @@
+package blockchainprojectpackage;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.*;
+
+public class LoginPanel extends JFrame implements ActionListener {
+	//public JFrame frame = new JFrame();
+	public JPanel panel = new JPanel();
+	public JLabel label = new JLabel("test");
+	public JLabel passLabel = new JLabel("Password");
+	public JTextField passField = new JTextField(20);
+	public JButton loginButton = new JButton("Login");
+	private JButton resultsButton = new JButton("View Results");
+	
+	
+	LoginPanel()
+	{
+		resultsButton.setVisible(Boolean.FALSE);
+		
+		//SETTING BOUNDS FOR ALL THE BUTTONS IN THE FRAME.
+		passField.setBounds(100, 20, 180, 30);
+		loginButton.setBounds(290, 20, 80, 30);
+		passLabel.setBounds(35, 18, 60, 30);
+		resultsButton.setBounds(120, 80, 150, 40);
+		panel.setBounds(0, 0, 400, 200);
+		
+		//ADDING ELEMENTS TO THE PANEL AND SETTING THE DEFAULT PROTOCOL OF THE FRAME.
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400,200);
+		panel.setLayout(null);
+		panel.add(passLabel);
+		panel.add(passField);
+		panel.add(loginButton);
+		panel.add(resultsButton);
+		setVisible(true);
+		add(panel);
+		
+		//--------------------loginButton EVENT HANDLING----------------------------------
+		loginButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				String password = passField.getText();			
+				
+				if(password.equals("admin"))
+				{
+					resultsButton.setVisible(Boolean.TRUE);
+				}
+			}
+		});	
+		
+		//Add a second action listener for the enter key.
+		KeyStroke enterButton = KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0, false);
+		passField.getInputMap().put(enterButton, "ENTER"); 
+		passField.getActionMap().put("ENTER", new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				String password = passField.getText();	
+				if(password.equals("admin"))
+				{
+					resultsButton.setVisible(Boolean.TRUE);
+				}
+			}
+		});
+	
+		//---------------------resultsButton EVENT HANDLING----------------------
+		resultsButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e) {
+				System.out.print("test"); //PLACEHOLDER
+				//Add event handling so that when the button gets pressed it opens the frame that Christian was working on.
+			}
+		});
+	}
+}
---
+++ StartFrame.java
@@ -0,0 +1,76 @@
+package blockchainprojectpackage;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class StartFrame extends JFrame {
+	public JPanel mainPanel = new JPanel();
+	public JLabel mainLabel = new JLabel("Menu");
+	public JButton loginButton = new JButton("Start Voting");
+	public JButton adminButton = new JButton("Admin Login");
+	public JButton infoButton = new JButton("Enter Candidate Info");
+	public JTextField passField = new JTextField(20);
+	
+	
+	//CHANGE SET BOUNDS OF EACH BUTTON AND MENU JLABEL SO START FRAME LOOKS NICE.
+	StartFrame(BlockchainFrame mainFrame, Blockchain mainChain){
+		super("CBlock");
+		setSize(535, 150);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(null);
+		
+		mainLabel.setBounds(250, 10, 40, 20);
+		loginButton.setBounds(10, 40, 160, 30);
+		adminButton.setBounds(180, 40, 160, 30);
+		infoButton.setBounds(350, 40, 160, 30);
+		
+		add(mainLabel);
+		add(loginButton);
+		
+		//-----------------------ADMIN BUTTON--------------------------------------
+		//adds and sets the listener for the admin button to go to the login panel
+		adminButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				if(e.getSource() == adminButton)
+				{
+					
+					LoginPanel newAdmin = new LoginPanel();
+				}
+			}
+		});
+		
+		//---------------------------------VOTE BUTTON-----------------------------
+		add(adminButton);
+		//loginButton.setBounds(200,200,180,85);
+		loginButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource() == loginButton) {
+					//BlockchainFrame updatedFrame = new BlockchainFrame(mainChain);
+					mainFrame.setVisible(true);
+					mainFrame.setSize(450, 400);
+					//System.out.println("dog");
+				}
+			}
+		});
+		
+		//-------------------------CANDIDATE INFO PANEL----------------------------
+		
+		add(infoButton);
+		infoButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() == infoButton) {
+						CandidateInfoPanel adminInfoPanel = new CandidateInfoPanel(mainFrame, mainChain);
+						adminInfoPanel.setVisible(true);
+						adminInfoPanel.setSize(380, 320);
+				}
+			}
+		});
+		
+		
+		
+	}
+}
